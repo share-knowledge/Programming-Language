83faf4b6317b5338f7c59e72cf46a346103013e4
steven <zenggang@bangwork.com>
2016-09-08 11:23:57 +0800

    Fix bug 测试 #5
    测试commit

---
+++ go/leanCloud_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+const (
+	appId     = "9xVN1YRyCGahfIaYkTJR1xVI-gzGzoHsz"
+	appKey    = "CcA3gIennSg0jjCjLkcAl2sl"
+	masterKey = "zAYxjv4IwNpczXdaWz16Gi7m"
+)
+
+func Test_time(t *testing.T) {
+
+}
