b2aeda1aa7cc355e5f93511b256508f776c315f1
steven <zenggang@bangwork.com>
2016-11-04 14:01:31 +0800

    Add

---
+++ C++/cmake_example3/lib/hello.cc
@@ -0,0 +1,8 @@
+#include "hello.h"
+#include <stdio.h>
+
+void HelloFunc()
+{
+	int a = 101;
+	printf("hello: %d\n", a);
+}
---
+++ C++/cmake_example3/lib/hello.h
@@ -0,0 +1,7 @@
+#ifndef cmake_lib_hello_h_
+#define cmake_lib_hello_h_
+
+
+void HelloFunc();
+
+#endif // cmake_lib_hello_h_
